0d5f55332b9c194edf8be554ef2180a5c3090e7c
subbu3012 <bhsubbu.1995@gmail.com>
2018-03-18 20:50:19 +0530

    Feat - Default route is changed to 'dashboard'

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,17 +4,11 @@ import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'dashboard',
         component: DashboardComponent,
-        // children: [
-        //     { path: 'reports', loadChildren: './reports/reports.module#ReportsModule' },
-        //     { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-        //     { path: 'itemnorm', loadChildren: './itemnorm/itemnorm.module#ItemnormModule' },
-        //     { path: 'stocknsales', loadChildren: './stocknsales/stocknsales.module#StockNSalesModule' },
-        //     { path: 'businessmap', loadChildren: './businessmap/businessmap.module#BusinessmapModule' },
-        //     { path: '', redirectTo: 'dashboard' },
-        //     { path: '**', redirectTo: 'dashboard' }
-        // ]
+    }, {
+        path: '',
+        redirectTo: 'dashboard'
     }
 ];
 
