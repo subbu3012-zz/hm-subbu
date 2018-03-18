61d6405c36110ea33284d74b12dc56f963ccf644
subbu3012 <bhsubbu.1995@gmail.com>
2018-03-18 20:50:57 +0530

    Feat - Snackbar and Progressbar feature are added to the common service

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -4,23 +4,29 @@ import { Observable } from 'rxjs/Rx';
 import { catchError, map, tap } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
 import { AngularFireDatabase, AngularFireList, } from 'angularfire2/database';
-import {  Upload, NoteMaster } from './dashboard.model';
+import { Upload, NoteMaster } from './dashboard.model';
 import * as firebase from 'firebase/app';
 import { FirebaseApp } from 'angularfire2'
+import { MatSnackBar } from '@angular/material';
 import 'firebase/storage';
 
 
 @Injectable()
 export class DashboardService {
 
+    public showProgressBar: boolean = false;
     noteList: AngularFireList<any>;
     private baseFileUploadPath: string = '/uploads';
 
-    constructor(public firebaseApp: FirebaseApp, private http: HttpClient, private firebaseDB: AngularFireDatabase) {
+    constructor(public snackBar: MatSnackBar, public firebaseApp: FirebaseApp, private http: HttpClient, private firebaseDB: AngularFireDatabase) {
 
     }
 
     getNoteList() {
+        this.showProgressBar = true;
+        setTimeout(() => {
+            this.showProgressBar = false;
+        }, 2000);
         this.noteList = this.firebaseDB.list('noteList/');
         return this.noteList;
     }
@@ -32,21 +38,17 @@ export class DashboardService {
             noteDesc: note.noteDesc,
             noteAttachment: note.noteAttachment,
             noteCreatedDate: new Date().toDateString()
-            // SSID: hotspot.SSID,
-            // latitude: hotspot.latitude,
-            // longitude: hotspot.longitude,
-            // name: hotspot.name,
-            // tag: hotspot.tag
         });
+        this.showProgressBar = false;
     }
 
     addNoteData(noteData: NoteMaster) {
+        this.showProgressBar = true;
         if (noteData.noteType == "picture" || noteData.noteType == "document") {
             let upload = noteData.noteAttachment;
             let storageRef = this.firebaseApp.storage().ref().child(`${this.baseFileUploadPath}/${upload.file.name}`);
 
             storageRef.put(upload.file).then(snapshot => {
-                console.log('successfully added', snapshot.downloadURL);
                 upload.url = snapshot.downloadURL
                 upload.name = upload.file.name
                 this.addNewNote(noteData);
@@ -56,48 +58,9 @@ export class DashboardService {
         }
     }
 
-    pushUpload(upload: Upload) {
-        let storageRef = firebase.storage().ref();
-        let uploadTask = storageRef.child(`${this.baseFileUploadPath}/${upload.file.name}`).put(upload.file);
-
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-            (snapshot: any) => {
-                // upload in progress
-                upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            },
-            (error) => {
-                // upload failed
-                console.log(error)
-            },
-            () => {
-                // upload success
-            }
-        );
-    }
-
-
-    // Writes the file details to the realtime db
-    private saveFileData(upload: Upload) {
-        this.firebaseDB.list(`${this.baseFileUploadPath}/`).push(upload);
-    }
-
-    deleteUpload(upload: Upload) {
-        this.deleteFileData(upload.$key)
-            .then(() => {
-                this.deleteFileStorage(upload.name)
-            })
-            .catch(error => console.log(error))
-    }
-
-    // Deletes the file details from the realtime db
-    private deleteFileData(key: string) {
-        return this.firebaseDB.list(`${this.baseFileUploadPath}/`).remove(key);
-    }
-
-    // Firebase files must have unique names in their respective storage dir
-    // So the name serves as a unique key
-    private deleteFileStorage(name: string) {
-        let storageRef = firebase.storage().ref();
-        storageRef.child(`${this.baseFileUploadPath}/${name}`).delete()
+    public openSnackBar(message: string, action: string) {
+        this.snackBar.open(message, action, {
+            duration: 2000,
+        });
     }
 }
